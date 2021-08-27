105aa7b07bb217012d3e58d431b230b39d19695f
Pratap Dessai <pratap.dessai@MBP-C02Z95D3LVDL.leboncoin.loc>
2021-08-27 21:57:59 +0200

    fix(genericRouteComponents): PAT-24 generic components

--- src/pages/Placeholder/Placeholder.tsx
+++ src/pages/Placeholder/Placeholder.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Placeholder.module.scss';
 
 const Placeholder = () => {
-  return <div className={styles.container} />;
+  return <div className={styles.container}>Placeholder</div>;
 };
 
 export default Placeholder;
